d0b49593daf25159067051ca4eef9a81cef191e3
ImogeneKaufman <imogene.kaufman@gmail.com>
2015-08-26 18:10:16 +0000

    git rm bundle

--- .bundle/gems/google-translate-1.1.2/lib/google_translate/version.rb
+++
@@ -1,3 +0,0 @@
-class GoogleTranslate
-  VERSION = "1.1.2"
-end
--- .bundle/gems/speech2text-0.3.6/lib/speech.rb
+++
@@ -1,9 +0,0 @@
-# -*- encoding: binary -*-
-require 'curb'
-require 'json'
-
-module Speech; end
-
-require 'speech/audio_inspector'
-require 'speech/audio_splitter'
-require 'speech/audio_to_text'
